4ded96cf79a3538f549f870704d5d7d53be510ce
Sudhanva Shashidhara <sudhanvashashidhara@gmail.com>
2021-05-20 09:01:08 +0530

    Removed Cookie Bot from sapper

--- src/routes/privacy-policy.svelte
+++ src/routes/privacy-policy.svelte
@@ -1,5 +1,5 @@
-<script
+<!-- <script
   id="CookieDeclaration"
   src="https://consent.cookiebot.com/4b69cf11-9241-45ea-bd1b-71506e836eed/cd.js"
   type="text/javascript"
-  async></script>
+  async></script> -->
